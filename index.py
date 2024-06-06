46923cfc3e13bed38dafce4ea30305a057d4ed89
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-05 22:10:21 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
+from translate import Translator
 
+translator = Translator(to_lang="pt")  
+
+
+texto = str(input('Insira uma frase para traduzir para pt/br: '))
+
+
+traducao = translator.translate(texto)
+
+
+print(f"Texto original: {texto}")
+print(f"Tradução para o Português: {traducao}")
